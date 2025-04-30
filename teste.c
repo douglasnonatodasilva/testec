dce8849ffc8e737c0dc005a90a66fdae8504102f
douglasnonatodasilva <dougns69@gmail.com>
2025-04-30 00:00:17 +0000

    while do while for

--- teste.c
+++ teste.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int numero, i;
+
+    printf("digite um numero para calcularmos a tabuada...\n");
+    scanf("%d", &numero);
+
+    for(i = 0; i <= 10; i++)
+    {
+        printf("%d x %d = %d \n", i, numero, i * numero);
+    }
+
+
+
+
+return 0;
+
+}
+
+
+
 
